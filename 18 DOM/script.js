2c8d5f747cdf78ac832c8b8eb13724720ea517c4
claudinei messias <claudineimessias@hotmail.com>
2021-11-24 16:38:22 -0300

    update

---
+++ 18 DOM/script.js
@@ -0,0 +1,18 @@
+
+// MANIPULANDO CONTEÚDOS
+
+//InnerHTML
+
+// 1  Serve para alterar o conteúdo de texto elemento
+
+const one = document.querySelector(".one");
+one.innerHTML = "Olá dev!";
+
+// 2 serve para criar elementos HTML na página
+
+const demo = document.querySelector("#demo")
+
+demo.innerHTML="<h2>Ola dev2</h2>";
+
+
+
